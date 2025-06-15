7902a3c260fe176532a859c83eff65e2267045c6
kengenal <kengenal1@gmail.com>
2025-06-15 16:00:23 +0200

    Add codespell to sql nvim

--- .config/sql-nvim/after/plugin/nonels.lua
+++ .config/sql-nvim/after/plugin/nonels.lua
@@ -1,14 +1,11 @@
 local null_ls = require("null-ls")
-local methods = require("null-ls.methods")
-local h = require("null-ls.helpers")
-
-
 
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.sql_formatter,
         -- Spell check
         null_ls.builtins.completion.spell,
+        null_ls.builtins.diagnostics.codespell,
     },
 })
 
