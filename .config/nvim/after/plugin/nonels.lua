13e6c5cbee5eec5fb34f9bada325b87f894b2e3d
kengenal <kengenal1@gmail.com>
2025-06-16 13:01:52 +0200

    Add format selected lines

--- .config/nvim/after/plugin/nonels.lua
+++ .config/nvim/after/plugin/nonels.lua
@@ -19,3 +19,12 @@ null_ls.setup({
 })
 
 vim.keymap.set("n", "<leader>l", "<cmd>lua vim.lsp.buf.format({async = true})<CR>")
+vim.keymap.set("v", "<leader>l", function()
+  vim.lsp.buf.format({
+    range = {
+      ["start"] = vim.api.nvim_buf_get_mark(0, "<"),
+      ["end"] = vim.api.nvim_buf_get_mark(0, ">"),
+    },
+    async = true,
+  })
+end, { noremap = true, silent = true, desc = "Format selected text" })
