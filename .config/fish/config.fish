35e9eebd3b5ceadc411e4ba0c4494c3ece1ac608
kengenal <kengenal1@gmail.com>
2025-09-23 08:27:25 +0200

    Fix fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    starship init fish | source
+
+    pyenv init - | source
+    fzf --fish | source
+
+
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim
+
 end
+
+
